2282485d274a249e6e6e4ea81a2c6feebc2af6e5
Adam Howard <adam.thomas.howard@gmail.com>
2025-11-10 12:54:57 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use clap::{Arg, ArgAction, Command};
+use encre_css::Config;
+
+fn main() {
+    let matches = Command::new("encre-compile")
+        .about("Compiles simple class string into CSS")
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::new("full")
+                .short('f')
+                .long("full")
+                .help("Build full CSS, including basic configuration.")
+                .action(ArgAction::SetTrue),
+        )
+        .trailing_var_arg(true)
+        .arg(
+            Arg::new("CLASSES")
+                .help("Specify CSS classes to compile.")
+                .num_args(1..),
+        )
+        .get_matches();
+
+    let classes: String = matches
+        .get_many::<String>("CLASSES")
+        .unwrap_or_default()
+        .map(|s| s.to_string())
+        .reduce(|acc, item| format!("{} {}", acc, item))
+        .unwrap_or_default();
+
+    let config = Config::default();
+    let generated_config = encre_css::generate([r#"<div class=""></div>"#], &config);
+    let generated = encre_css::generate(
+        [format!("<div class=\"{}\"></div>", classes).as_str()],
+        &config,
+    );
+
+    let diff = generated.replace(generated_config.as_str(), "");
+    println!("{}", diff.trim());
+}
